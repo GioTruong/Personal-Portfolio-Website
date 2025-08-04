82f9d6f97b7001ce4ffaff4a5ead311e9d4ac6e9
GioTruong <truonggioitridungtv23@gmail.com>
2025-08-04 10:37:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+var typed = new Typed(".typing", {
+    strings:["Software Engineer","Software Developer", "Web Designer", "Web Developer", "Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
